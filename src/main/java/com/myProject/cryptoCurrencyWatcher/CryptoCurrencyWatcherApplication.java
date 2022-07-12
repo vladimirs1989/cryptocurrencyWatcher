d437008a40a5aa971cea2c70eceb1333c1239ff7
vladimirs1989 <sitnik_vladimir89@mail.ru>
2022-07-12 21:17:45 +0300

    creating a project for Spring

---
+++ src/main/java/com/myProject/cryptoCurrencyWatcher/CryptoCurrencyWatcherApplication.java
@@ -0,0 +1,13 @@
+package com.myProject.cryptoCurrencyWatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoCurrencyWatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoCurrencyWatcherApplication.class, args);
+	}
+
+}
