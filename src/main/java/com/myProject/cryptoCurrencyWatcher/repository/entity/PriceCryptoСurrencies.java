f70b17967d61fec5e3cfc1674f494d9fc024a66d
vladimirs1989 <sitnik_vladimir89@mail.ru>
2022-07-13 13:55:05 +0300

    creating tables

--- src/main/java/com/myProject/cryptoCurrencyWatcher/repository/entity/PriceCryptoСurrencies.java
+++ src/main/java/com/myProject/cryptoCurrencyWatcher/repository/entity/PriceCryptoСurrencies.java
@@ -11,7 +11,7 @@ import java.sql.Timestamp;
 import java.util.Objects;
 
 @Entity
-@Table (name = "pricecryptocurrencies")
+@Table (name = "prices")
 public class PriceCryptoСurrencies {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
